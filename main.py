1d1c1f47f0cd723693c10af7b962c50cabdc08f5
Gandlin Alexander <137958267+GandlinAlexandr@users.noreply.github.com>
2024-12-02 07:22:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-from sklearn.linear_model import Ridge
 import pandas as pd
 import numpy as np
 import uvicorn
@@ -11,6 +10,9 @@ import category_encoders as ce
 import sklearn
 sklearn.set_config(transform_output="pandas")
 from joblib import load
+from fastapi import File, UploadFile
+from fastapi.responses import StreamingResponse
+import io
 
 
 def transform_data(dataset):
@@ -23,7 +25,6 @@ def transform_data(dataset):
                                                   else x)
     dataset['engine'] = dataset['engine'].apply(lambda x: float(x.split(' ')[0]) if type(x)!=float else x)
     dataset['max_power'] = dataset['max_power'].apply(lambda x: float(x.split(' ')[0]) if type(x)!=float else x)
-    dataset['engine'] = dataset['engine'].astype(int)
     dataset['name'] = dataset['name'].apply(lambda x: x.split(' ')[0])
     # Возвращение преобразованного датафрейма
     return dataset
@@ -42,7 +43,7 @@ def fill_NA(dataset, train_median_mileage, train_median_engine, train_median_max
 app = FastAPI()
 
 # Загрузка данных
-df_train = pd.read_csv('https://raw.githubusercontent.com/Murcha1990/MLDS_ML_2022/main/Hometasks/HT1/cars_train.csv').dropna()
+df_train = pd.read_csv('https://raw.githubusercontent.com/Murcha1990/MLDS_ML_2022/main/Hometasks/HT1/cars_train.csv')
 y_train = df_train['selling_price']
 X_train = transform_data(df_train.drop(['selling_price', 'torque'], axis=1))
 
@@ -56,7 +57,7 @@ train_median_seats = X_train['seats'].median()
 X_train = fill_NA(X_train, train_median_mileage, train_median_engine, train_median_max_power, train_median_seats)
 
 # Кодирование name
-tgt_enc = ce.TargetEncoder(cols=['name'], smoothing=1)
+tgt_enc = ce.TargetEncoder(cols=['name'], smoothing=15)
 tgt_enc.fit(X_train, y_train)
 X_train = tgt_enc.transform(X_train)
 
@@ -73,8 +74,6 @@ scaler.fit(X_train_encoded)
 X_train_scaler = pd.DataFrame(scaler.transform(X_train_encoded), columns=X_train_encoded.columns)
 
 # Загрузка модели
-#model = Ridge(alpha=0.7)
-#.fit(X_train_scaler, y_train)
 model = load('model.pkl')
 
 print('http://127.0.0.1:8000/docs')
@@ -103,8 +102,8 @@ def predict_item(item: Item) -> float:
     # Конвертация в датафрейм и удаление ненужных столбцов
     input_data = pd.DataFrame([item.dict()]).drop(['selling_price', 'torque'], axis=1)
     # Обработка
+    input_data = fill_NA(input_data, train_median_mileage, train_median_engine, train_median_max_power, train_median_seats)
     data = transform_data(input_data)
-    data = fill_NA(data, train_median_mileage, train_median_engine, train_median_max_power, train_median_seats)
     # Кодирование name
     data = tgt_enc.transform(data)
     # OHE
@@ -121,7 +120,6 @@ def predict_item(item: Item) -> float:
     # Возврат предсказанного значения
     return float(round(np.e**predicted_price[0], 0))
 
-
 @app.post("/predict_items")
 def predict_items(items: Items) -> List[float]:
     # Конвертация в датафрейм и удаление ненужных столбцов
@@ -129,8 +127,8 @@ def predict_items(items: Items) -> List[float]:
     input_data = pd.DataFrame(input_data['objects'])
     input_data = input_data.drop(['selling_price', 'torque'], axis=1)
     # Обработка
-    data = transform_data(input_data)
-    data = fill_NA(data, train_median_mileage, train_median_engine, train_median_max_power, train_median_seats)
+    data = fill_NA(input_data, train_median_mileage, train_median_engine, train_median_max_power, train_median_seats)
+    data = transform_data(data)
     # Кодирование name
     data = tgt_enc.transform(data)
     # OHE
@@ -146,6 +144,40 @@ def predict_items(items: Items) -> List[float]:
     predicted_prices = model.predict(data_cat_encoded_scal.values)
     # Возврат списка предсказанных значений
     return [float(round(price, 0)) for price in np.e**predicted_prices]
+
+@app.post("/predict_item_csv")
+def upload(file: UploadFile = File(...)):
+    input_data = pd.read_csv(file.file, index_col=0)
+    # Конвертация в датафрейм и удаление ненужных столбцов
+    data = input_data.drop(['selling_price', 'torque'], axis=1)
+    # Обработка
+    data = fill_NA(data, train_median_mileage, train_median_engine, train_median_max_power, train_median_seats)
+    data = transform_data(data)
+    # Кодирование name
+    data = tgt_enc.transform(data)
+    # OHE
+    encoded_data = encoder.transform(data.select_dtypes('object'))
+    encoded_test_df = pd.DataFrame(encoded_data,
+                                   columns=encoder.get_feature_names_out(data.select_dtypes('object').columns))
+    data_cat_encoded = pd.concat(
+        [data.select_dtypes(exclude='object').reset_index(drop=True), encoded_test_df.reset_index(drop=True)],
+        axis=1)
+    # Скалирование
+    data_cat_encoded_scal = pd.DataFrame(scaler.transform(data_cat_encoded), columns=data_cat_encoded.columns)
+    # Предсказания модели
+    predicted_prices = model.predict(data_cat_encoded_scal.values)
+    # Возвращаемся от логарифмированного таргета к изначальным масштабам
+    predicted_prices = [float(round(price, 0)) for price in np.e ** predicted_prices]
+    # Добавление предсказанных значений в изначальный датасет
+    output = input_data.copy()
+    output['predict'] = predicted_prices
+    stream = io.StringIO()
+    output.to_csv(stream, index=False)
+    response = StreamingResponse(iter([stream.getvalue()]),
+                                 media_type="text/csv"
+                                 )
+    response.headers["Content-Disposition"] = "attachment; filename=predict.csv"
+    return response
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
